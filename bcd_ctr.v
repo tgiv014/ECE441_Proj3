701d28c7b186e07fb583451af8bc55ec4a41c369
Thomas Gorham <tgiv014@gmail.com>
2017-03-08 11:33:18 -0600

    Divorce the state machine and the bcd counter.
    Gave the bcd counter three inputs: clk, ar, and en.
    Will be much easier to control overall behavior.
    Also, timer freezes when it hits 999 - Makes sense in a one-shot application.

--- bcd_ctr.v
+++ bcd_ctr.v
@@ -2,93 +2,66 @@
 Thomas Gorham
 ECE 441 Spring 2017
 Project 2 - bcd_ctr
-Description: This module implements a 3 digit
-bcd counter with FSM that 
+Description: This module implements a 3-digit
+bcd counter with async reset and enable inputs.
+It is intended to be controlled by a state machine
+that manages inputs from the lfsr and pushbuttons
 ================================================*/
 `timescale 100 ns / 1 ns
 
 
-module bcd_ctr(clk, ar, start, stop, dig1, dig2, dig3, state);
+module bcd_ctr(clk, en, ar, dig1, dig2, dig3);
   /*======================================
   Parameters
   ======================================*/
-  parameter IDLE = 0'b0;
-  parameter RUN = 0'b1;
 
   /*======================================
   Input/Output Declaration
   ======================================*/
-  input clk, ar, start, stop;
+  input clk, ar, en;
   output reg  [3:0]  dig1, dig2, dig3;
-  output state;
+
   /*======================================
   Internal wires/registers
   ======================================*/
-  reg state;
+  wire dig1_carry, dig2_carry, dig3_carry;
 
   /*======================================
   Asynchronous Logic
   ======================================*/
-  assign dig1_carry = (dig1 == 4'd9);
-  assign dig2_carry = dig1_carry&(dig2 == 4'd9);
-  assign dig3_carry = dig2_carry&(dig3 == 4'd9);
+  assign dig1_carry = (dig1 == 4'd9); // Flag to indicate when to inc dig2
+  assign dig2_carry = dig1_carry&(dig2 == 4'd9); // Indicates when to inc dig3
+  assign dig3_carry = dig2_carry&(dig3 == 4'd9); // Indicates when timer should freeze
 
   /*======================================
   Synchronous Logic
   ======================================*/
   always @ (posedge clk or negedge ar)
   begin
-      if(~ar) // If reset has negedge down to level 0,
-      begin
-      state <= IDLE;
-
+    if(~ar) // If ar is brought low, (or is low on a clk posedge)
+    begin
+      // Reset the current count
       dig1 <= 4'd0;
       dig2 <= 4'd0;
       dig3 <= 4'd0;
+    end else if(~dig3_carry&en) // Only run the counter if en high and not at 999
+    begin
+      if(dig2_carry) // If dig2 and dig1 are 9's
+      begin
+        dig3 <= dig3 + 1; // Increment dig3
+        dig2 <= 0; // Reset the lower digits
+        dig1 <= 0;
+
+      end else if(dig1_carry) // If dig1 is a 9
+      begin
+        dig2 <= dig2 + 1; // Increment dig2
+        dig1 <= 0; // Reset lower digits
+
+      end else // If no carry-magic had to occur
+      begin
+        dig1 <= dig1 + 1; // Count normally
       end
-      else begin
-        // Not in reset - Service state machine
-        case(state)
-          // IDLE STATE - Only waiting for the start button
-          IDLE: begin
-            if(start)
-            begin
-            state<=RUN;
-            dig1 <= 4'd0;
-            dig2 <= 4'd0;
-            dig3 <= 4'd0;
-            end
-          end
+    end
+  end
 
-          // RUN STATE - Running Timer -> Returns to idle on stop
-          default: begin // Using default - But really we care about RUN
-            if(stop)
-            begin
-            state<=IDLE;
-            end
-
-            if(dig3_carry)
-            begin
-              state<=IDLE;
-            end
-            else if(dig2_carry)
-            begin
-              dig3 <= dig3 + 1;
-              dig2 <= 0;
-              dig1 <= 0;
-            end
-            else if(dig1_carry)
-            begin
-              dig2 <= dig2 + 1;
-              dig1 <= 0;
-            end
-            else
-            begin
-              dig1 <= dig1 + 1;
-            end
-          end
-          endcase
-          end
-          end
-
-          endmodule
+  endmodule
